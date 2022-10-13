34413d513c235fb2a17f22d5fe044fe8f4fb5e0d
Atul Kumar <37495003+Atulhackit@users.noreply.github.com>
2022-10-13 18:10:43 +0530

    groundoverlay added react ground overlay

--- index.ts
+++ index.ts
@@ -5,8 +5,8 @@
  */
 
 function initMap() {
-  // const myLatlng = { lat: 29.42698, lng: -98.43749 };
-  const myLatlng = { lat: 28.49735, lng: 77.1768 };
+  const myLatlng = { lat: 29.42698, lng: -98.43749 };
+  // const myLatlng = { lat: 28.49735, lng: 77.1768 };
 
   const map = new google.maps.Map(document.getElementById("map")!, {
     zoom: 20,
@@ -36,36 +36,35 @@ function initMap() {
 
     infoWindow.open(map);
   });
-  //   var kmlUrl =
-  //     "https://floorplans.s3.ap-south-1.amazonaws.com/tataplay-smartwarehouse/tataplay-floorplan.jpg";
-  //   // "https://spurs-floorplans.s3.ap-southeast-1.amazonaws.com/KML/L1-Event.kml";
-  //   // var kmlLayer = new google.maps.KmlLayer({ url: kmlUrl });
+  var kmlUrl =
+    "https://floorplans.s3.ap-south-1.amazonaws.com/tataplay-smartwarehouse/tataPlay.kml";
+  // "https://spurs-floorplans.s3.ap-southeast-1.amazonaws.com/KML/L1-Event.kml";
+  // var kmlLayer = new google.maps.KmlLayer({ url: kmlUrl });
 
-  //   const kmlLayer = new google.maps.KmlLayer({
-  //     url: kmlUrl,
-  //     suppressInfoWindows: true,
-  //     clickable: false,
-  //     screenOverlays: false
-  //     // map: map,
-  //   });
-  //   // add layer to map
-  //   kmlLayer.setMap(map);
-  // }
-
-  const imageBounds = {
-    north: 28.49768,
-    south: 28.49654,
-    east: 77.17722,
-    west: 77.17644
-  };
-
-  let historicalOverlay = new google.maps.GroundOverlay(
-    "https://floorplans.s3.ap-south-1.amazonaws.com/tataplay-smartwarehouse/tataplay-floorplan.jpg",
-    imageBounds,
-    { screenOverlays: false }
-  );
-  historicalOverlay.setMap(map);
+  const kmlLayer = new google.maps.KmlLayer({
+    url: kmlUrl,
+    suppressInfoWindows: true,
+    clickable: false,
+    screenOverlays: false
+    // map: map
+  });
+  // add layer to map
+  kmlLayer.setMap(map);
 }
+
+// this code will not work in this project its for refernce only
+// <GroundOverlay
+//                 url={
+//                   "https://floorplans.s3.ap-south-1.amazonaws.com/tataplay-smartwarehouse/tataplay-floorplan.jpg"
+//                 }
+//                 bounds={Restriction}
+//                 onClick={ev => {
+//                   console.log("latitide = ", ev.latLng.lat());
+//                   console.log("longitude = ", ev.latLng.lng());
+//                   alert("Lat " + ev.latLng.lat() + " | Lang " + ev.latLng.lng());
+//                 }}
+//               />
+
 declare global {
   interface Window {
     initMap: () => void;
