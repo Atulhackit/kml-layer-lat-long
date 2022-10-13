c0b1e0086477b291041fab450bab386baee1041f
Atul Kumar <37495003+Atulhackit@users.noreply.github.com>
2022-10-13 15:44:05 +0530

    using .jbp GraoundOverlay

--- index.ts
+++ index.ts
@@ -5,10 +5,11 @@
  */
 
 function initMap() {
-  const myLatlng = { lat: 29.42698, lng: -98.43749 };
+  // const myLatlng = { lat: 29.42698, lng: -98.43749 };
+  const myLatlng = { lat: 28.49735, lng: 77.1768 };
 
   const map = new google.maps.Map(document.getElementById("map")!, {
-    zoom: 11,
+    zoom: 20,
     center: myLatlng
   });
 
@@ -35,21 +36,36 @@ function initMap() {
 
     infoWindow.open(map);
   });
-  var kmlUrl =
-    "https://spurs-floorplans.s3.ap-southeast-1.amazonaws.com/KML/L1-Event.kml";
-  // var kmlLayer = new google.maps.KmlLayer({ url: kmlUrl });
+  //   var kmlUrl =
+  //     "https://floorplans.s3.ap-south-1.amazonaws.com/tataplay-smartwarehouse/tataplay-floorplan.jpg";
+  //   // "https://spurs-floorplans.s3.ap-southeast-1.amazonaws.com/KML/L1-Event.kml";
+  //   // var kmlLayer = new google.maps.KmlLayer({ url: kmlUrl });
 
-  const kmlLayer = new google.maps.KmlLayer({
-    url: kmlUrl,
-    suppressInfoWindows: true,
-    clickable: false,
-    screenOverlays: false
-    // map: map,
-  });
-  // add layer to map
-  kmlLayer.setMap(map);
+  //   const kmlLayer = new google.maps.KmlLayer({
+  //     url: kmlUrl,
+  //     suppressInfoWindows: true,
+  //     clickable: false,
+  //     screenOverlays: false
+  //     // map: map,
+  //   });
+  //   // add layer to map
+  //   kmlLayer.setMap(map);
+  // }
+
+  const imageBounds = {
+    north: 28.49768,
+    south: 28.49654,
+    east: 77.17722,
+    west: 77.17644
+  };
+
+  let historicalOverlay = new google.maps.GroundOverlay(
+    "https://floorplans.s3.ap-south-1.amazonaws.com/tataplay-smartwarehouse/tataplay-floorplan.jpg",
+    imageBounds,
+    { screenOverlays: false }
+  );
+  historicalOverlay.setMap(map);
 }
-
 declare global {
   interface Window {
     initMap: () => void;
